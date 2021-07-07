c00a719f496f122a0166653917fb75439b90c76f
claubermartins <clauber789@gmail.com>
2021-07-07 17:07:07 -0300

    Iniciando o projeto

---
+++ src/main/java/com/swapapi/SwapApiApplication.java
@@ -0,0 +1,13 @@
+package com.swapapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwapApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swapapi/SwapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
